7591aaa5b396d75de4f97f5b10c4c9b4dac56723
hn69969669 <hn69969669@gmail.com>
2025-01-10 19:13:24 +0700

    update 	modified:   Centralparktower.html 	modified:   Featurelist.html 	modified:   assets/css/Centralparktower.css 	modified:   assets/css/styles.css 	modified:   components/header.html 	modified:   js/LoadComponent.js

--- js/LoadComponent.js
+++ js/LoadComponent.js
@@ -5,3 +5,27 @@ function loadScript(component, elementId) {
       document.getElementById(elementId).innerHTML = data;
     });
 }
+
+// Remove or comment out the initializeModalHandlers function
+/*
+function initializeModalHandlers() {
+  const modals = ['signin', 'signup'].map(type => 
+    new bootstrap.Modal(document.getElementById(`${type}Modal`))
+  );
+  
+  document.querySelectorAll('[data-bs-toggle="modal"]').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const target = btn.getAttribute('data-bs-target').replace('#', '');
+      const modal = modals.find(m => m._element.id === target);
+      if(modal) modal.show();
+    });
+  });
+}
+
+// Update your myFunction to call this after loading components
+function myFunction() {
+  loadScript("/components/header.html", "header");
+  loadScript("/components/footer.html", "footer");
+  setTimeout(initializeModalHandlers, 100); // Give components time to load
+}
+*/
